eb8034697ab0bc19b9646f6ce3645afeec302fe4
user <fitymaalezredes@fidesz.gay>
2026-02-19 20:43:20 +0100

    Removing some unused stuff

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/nekosapi': {
-                target: 'https://api.nekosapi.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/nekosapi/, ''),
-            },
-        },
-    },
-    preview: {
-        proxy: {
-            '/nekosapi': {
-                target: 'https://api.nekosapi.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/nekosapi/, ''),
-            },
-        },
-    },
 });
